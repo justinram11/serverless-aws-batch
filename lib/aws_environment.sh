ce774ab6eb0e2ebb15426126531636e4ff17364d
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2020-01-16 02:11:17 +0900

    add credentials environment

--- lib/aws_environment.sh
+++ lib/aws_environment.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo $0
+echo $1
 echo $2
-export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .event.AWS_ACCESS_KEY_ID)
-export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .event.AWS_SECRET_ACCESS_KEY)
-export AWS_SESSION_TOKEN=$(echo $2 | jq -r .event.AWS_SESSION_TOKEN)
+echo $3
+echo $4
+export AWS_ACCESS_KEY_ID=$(echo $2 | jq -r .AWS_ACCESS_KEY_ID)
+export AWS_SECRET_ACCESS_KEY=$(echo $2 | jq -r .AWS_SECRET_ACCESS_KEY)
+export AWS_SESSION_TOKEN=$(echo $2 | jq -r .AWS_SESSION_TOKEN)
 echo ${AWS_ACCESS_KEY_ID}
 
 /var/lang/bin/python3.6 /var/runtime/awslambda/bootstrap.py $1
