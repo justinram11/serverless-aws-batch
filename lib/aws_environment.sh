6dc6332add8043f18bcc3384ad026d2dd70e0ae3
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2020-01-16 19:42:37 +0900

    add credentials environment

--- lib/aws_environment.sh
+++ lib/aws_environment.sh
@@ -4,7 +4,7 @@ echo $1
 echo $2
 credentials=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
 export AWS_ACCESS_KEY_ID=$(echo $credentials | jq -r .AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo $credentials | jq -r .SECRET_ACCESS_KEY)
+export AWS_SECRET_ACCESS_KEY=$(echo $credentials | jq -r .SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $credentials | jq -r .Token)
 echo ${AWS_ACCESS_KEY_ID}
 
